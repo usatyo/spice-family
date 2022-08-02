837db7f08782726fc7af5ac3ca3d0a55235419ef
usatyo <yu0424.math8128@gmail.com>
2022-08-02 11:51:34 +0900

    make output

--- backend/correct_board.py
+++ backend/correct_board.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("images/diag_stone.jpg")
+image = cv2.imread("images/diag_stone2.png")
 image = cv2.blur(image, (3, 3))
 
 
-x = [90, 365, 47, 397]
-y = [8, 8, 270, 273]
-# x = [1050, 2020, 420, 2720]
-# y = [940, 950, 1640, 1630]
+# x = [90, 365, 47, 397]
+# y = [8, 8, 270, 273]
+x = [1050, 2020, 420, 2720]
+y = [940, 950, 1640, 1630]
 
 p1 = np.array([x[0], y[0]])
 p2 = np.array([x[1], y[1]])
@@ -27,7 +27,7 @@ for i in range(4):
     circle_image = cv2.circle(image, (x[i], y[i]), 10, (255, 0, 0), thickness=-1)
 
 
-cv2.imshow("image", circle_image)
-cv2.imshow("board_image", output)
-
-cv2.waitKey()
+# cv2.imshow("image", circle_image)
+# cv2.imshow("board_image", output)
+cv2.imwrite("images/output_diag_stone2.jpg", output)
+# cv2.waitKey()
--- backend/detect_board.py
+++ backend/detect_board.py
@@ -12,10 +12,15 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # ret, split = cv2.threshold(gray2, 80, 255, cv2.THRESH_BINARY)
 split = cv2.Canny(gray, 30, 300, L2gradient=True)
 dilate_image = cv2.dilate(split, neiborhood, iterations=2)
-erode_image = cv2.erode(dilate_image,neiborhood,iterations=3)
+erode_image = cv2.erode(dilate_image, neiborhood, iterations=3)
 
 lines = cv2.HoughLinesP(
-    erode_image, rho=1, theta=np.pi / 360, threshold=50, minLineLength=len(image[0]) // 10, maxLineGap=len(image[0]) // 20
+    erode_image,
+    rho=1,
+    theta=np.pi / 360,
+    threshold=50,
+    minLineLength=len(image[0]) // 10,
+    maxLineGap=len(image[0]) // 20,
 )
 
 print(lines)
@@ -25,5 +30,6 @@ for line in lines:
 
     red_line_img = cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-cv2.imshow("line", red_line_img)
-cv2.waitKey()
+# cv2.imshow("line", red_line_img)
+cv2.imwrite("images/output_diag_board2.jpg", red_line_img)
+# cv2.waitKey()
