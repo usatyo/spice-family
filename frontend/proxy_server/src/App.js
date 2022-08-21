3dab4d121d4b24feb8a48499f34706b3ad2e9bcf
Toyoshin <toyodasinngo@gmail.com>
2022-08-21 20:53:43 +0900

    Resize UI interactively

--- frontend/proxy_server/src/App.js
+++ frontend/proxy_server/src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Box, Button, Stack, HStack } from "@chakra-ui/react";
+import { AspectRatio, ChakraProvider, VStack } from '@chakra-ui/react';
+import { Box, Button, Stack, HStack, RectBox } from "@chakra-ui/react";
 
 
 function App() {
   return (
+
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
-          <HStack p={10} spacing={4}>
-            <Box p={4} bg={"white"} shadow="lg" rounded="30" width={500} height={700}></Box>
-            <Box p={4} bg={"white"} shadow="lg" rounded="30" width={500} height={700}></Box>
-          </HStack>
+          <AspectRatio w="full" ratio={16 / 9} >
+            <HStack p={8} spacing={4}>
+              <Box p={4} bg={"white"} shadow="lg" rounded={30} w={700} h="full" margin={"0%"}>
+              </Box>
+              <Box w="full" h="full" bg="green.100" >
+                {/* <VStack spacing={4}>
+
+
+                </VStack> */}
+              </Box>
+              <Box p={4} bg={"white"} shadow="lg" rounded={30} w={700} h="full" margin={"0%"}></Box>
+            </HStack>
+          </AspectRatio>
         </header>
       </div>
-    </ChakraProvider>
+    </ChakraProvider >
   );
 }
 
