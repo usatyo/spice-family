f9bf122d3b969a22c5fe02841120cee780077db7
USK314 <usk31101021@gmail.com>
2022-08-22 15:56:53 +0900

    feat: success the routing

--- frontend/proxy_server/src/App.js
+++ frontend/proxy_server/src/App.js
@@ -1,49 +1,31 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
-import { AspectRatio, ChakraProvider, Spacer, VStack } from '@chakra-ui/react';
-import { Box, Button, Stack, HStack, Flex, Text, Center } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home'
+import Timer from './pages/Timer'
+import Result from './pages/Result'
 
 function App() {
   var primaryColor = '#59A4CB';
   return (
-    <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <AspectRatio w="full" ratio={16 / 9} >
-            <HStack p={8} spacing={4}>
-              <Box p={4} bg={"white"} shadow="lg" rounded="20px" w="700px" h="full" margin={"0%"}>
-                <VStack h="full">
-                  <HStack w="full">
-                    <Box bg={"black"} shadow="lg" rounded="full" w="70px" h="30px" >
-                      <Center h='100%'>
-                        <Text fontSize="md" fontWeight="bold" color="white">先手</Text>
-                      </Center>
-                    </Box>
-                    <Spacer />
-                  </HStack>
-                  <Spacer />
-                  <Box>
-                    <Text fontSize="xl" fontWeight="bold" color="black">ここに時計</Text>
-                  </Box>
-                  <Spacer />
-                  <Button colorScheme="blue" variant="outline" w="full" borderRadius={10}>
-                    <Text fontSize="xl" fontWeight="bold" colorScheme="blue">着手</Text>
-                  </Button>
-                </VStack>
-              </Box>
-              <Box w="full" h="full" bg="green.100" >
-                {/* <VStack spacing={4}>
-
-
-                </VStack> */}
-              </Box>
-              <Box p={4} bg={"white"} shadow="lg" rounded={20} w={700} h="full" margin={"0%"}></Box>
-            </HStack>
-          </AspectRatio>
-        </header>
-      </div >
-    </ChakraProvider >
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/timer' element={<Timer />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const AppContainer = () => {
+  return (
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  )
+}
+
+export default AppContainer;
---
+++ frontend/proxy_server/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>hello, world</h1>
+    )
+}
+
+export default Home
