9a5139f62864ea8e8547095201e544c734e73bd4
USK314 <usk31101021@gmail.com>
2022-08-22 21:16:35 +0900

    create temporary modal

--- frontend/proxy_server/src/pages/Home.js
+++ frontend/proxy_server/src/pages/Home.js
@@ -1,7 +1,8 @@
+import React, {useState} from 'react';
 import { AspectRatio, ChakraProvider, Spacer, VStack } from '@chakra-ui/react';
 import { Box, Button, Stack, HStack, Flex, Text, Center, Image } from "@chakra-ui/react";
-import TimerSvg from '../assets/timer.svg'
-import MyChart from '../components/myChart'
+import MyChart from '../components/MyChart'
+import MyModal from '../components/MyModal'
 
 const Home = () => {
     return (
@@ -9,9 +10,7 @@ const Home = () => {
             <header className="App-header">
                 <VStack ml={100} mt={50}>
                     <HStack mb={30}>
-                        <Button p={4} bg={"white"} shadow="lg" rounded="20px" w="120px" h="120px">
-                            <Image src={TimerSvg} h={75}></Image>
-                        </Button>
+                        <MyModal />
                         <Box pl={270} pr={230} color={'#59A4CB'} fontSize="7xl" fontWeight="bold" >プロ棋士サーバ</Box>
                         <Spacer></Spacer>
                         <Box pt={3} pl={4} bg={"white"} color={"black"} fontSize={17} textAlign={"left"} shadow="lg" rounded="10px" w="180px" h="50px">北海道大学囲碁部</Box>
