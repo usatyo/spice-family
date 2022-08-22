144885be6b56c02cfae38b50fcf9771d5e9a9b44
USK314 <usk31101021@gmail.com>
2022-08-22 20:15:23 +0900

    create temporary graph

--- frontend/proxy_server/src/pages/Home.js
+++ frontend/proxy_server/src/pages/Home.js
@@ -1,22 +1,24 @@
 import { AspectRatio, ChakraProvider, Spacer, VStack } from '@chakra-ui/react';
 import { Box, Button, Stack, HStack, Flex, Text, Center, Image } from "@chakra-ui/react";
 import TimerSvg from '../assets/timer.svg'
-
+import MyChart from '../components/myChart'
 
 const Home = () => {
     return (
         <div className="App" >
             <header className="App-header">
                 <VStack ml={100} mt={50}>
-                    <HStack>
+                    <HStack mb={30}>
                         <Button p={4} bg={"white"} shadow="lg" rounded="20px" w="120px" h="120px">
                             <Image src={TimerSvg} h={75}></Image>
                         </Button>
                         <Box pl={270} pr={230} color={'#59A4CB'} fontSize="7xl" fontWeight="bold" >プロ棋士サーバ</Box>
                         <Spacer></Spacer>
-                        <Box pt={2} pl={5} bg={"white"} color={"black"} fontSize={20} textAlign={"left"} shadow="lg" rounded="10px" w="180px" h="50px">チーム別</Box>
+                        <Box pt={3} pl={4} bg={"white"} color={"black"} fontSize={17} textAlign={"left"} shadow="lg" rounded="10px" w="180px" h="50px">北海道大学囲碁部</Box>
                     </HStack>
-                    <Stack>aaa</Stack>
+                    <Stack p={12} bg={"white"} shadow="lg" rounded="20px" w="900px" h="450px">
+                        <MyChart />
+                    </Stack>
                 </VStack>
             </header>
         </div >
