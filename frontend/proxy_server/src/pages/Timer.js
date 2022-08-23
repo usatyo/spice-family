f07a1f441c18700b15b419db4c055dc5633b70ca
Toyoshin <toyodasinngo@gmail.com>
2022-08-23 18:21:05 +0900

    The screen design of the timer screen is completed

--- frontend/proxy_server/src/pages/Timer.js
+++ frontend/proxy_server/src/pages/Timer.js
@@ -19,63 +19,114 @@ const renderTime = ({ remainingTime }) => {
 
 const Timer = () => {
 
-  const sente = <Box bg={"black"} shadow="lg" rounded="full" w="80px" h="40px">
+  const sente = <Box bg={"black"} rounded="full" w="80px" h="40px">
     <Center h='100%'>
       <Text fontSize="md" fontWeight="bold" color="white">先手</Text>
     </Center>
   </Box>;
-  var timerSeconds = 40 * 60;
+  const gote = <Box bg={"white"} border='1px' borderColor='black' rounded="full" w="80px" h="40px">
+    <Center h='100%'>
+      <Text fontSize="md" fontWeight="bold" color="black">後手</Text>
+    </Center>
+  </Box>;
+
+  var timerSeconds = 1 * 60;
   return (
     <div className="App">
       <header className="App-header">
         <AspectRatio w="full" ratio={16 / 9} >
           <HStack p={8} spacing={4}>
-            <Box p={4} bg={"white"} shadow="lg" rounded="20px" w="1000px" h="full" margin={"0%"}>
+            {/* 左のプレイヤー */}
+            <Box p={4} bg={"white"} shadow="2xl" rounded="30px" w="1000px" h="full" margin={"0%"}>
               <VStack h="full">
                 <HStack w="full">
                   {sente}
                   <Spacer />
                 </HStack>
                 <Spacer />
-                <Box>
+                <Box shadow="xl" rounded=" full" padding={2}>
                   <CountdownCircleTimer
+                    size={250}
+                    strokeWidth={10}
                     isPlaying
                     duration={timerSeconds}
-                    colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-                    colorsTime={[10, 6, 3, 0]}
+                    colors={["#5a97db", "#5a97db", "#f4d849", "#A30000"]}
+                    colorsTime={[60, 20, 10, 0]}
                     onComplete={() => ({ shouldRepeat: true, delay: 1 })}
                   >
                     {renderTime}
                   </CountdownCircleTimer>
                 </Box>
+                <Box marginStart={10}>
+                  <Text as="span" fontSize={20} color="blue.400" fontWeight="bold">考慮回数 </Text>
+                  <Text as="span" fontSize={40} color="blue.400" fontWeight="bold">1</Text>
+                  <Text as="span" fontSize={20} color="blue.400" fontWeight="bold">回</Text>
+                </Box>
                 <Spacer />
-                <Button colorScheme="blue" variant="outline" w="full" h="50px" borderRadius={10}>
-                  <Text fontSize="xl" fontWeight="bold" colorScheme="blue">着手</Text>
+                <Button colorScheme="blue" shadow="lg" variant="outline" w="full" h="70px" borderRadius={20}>
+                  <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
                 </Button>
               </VStack>
             </Box>
+            {/* 真ん中のところ */}
             <Box w="full" h="full" >
-              <VStack spacing={4} h="full">
+              <VStack spacing={0} h="full">
                 {sente}
                 <Box>
                   <Text as="span" fontSize={60} color="blue.400" fontWeight="bold">1</Text>
                   <Text as="span" fontSize={30} color="blue.400" fontWeight="bold">手目</Text>
                 </Box>
-
                 <Box bg="white" shadow="lg" borderRadius={10} padding="10px">
                   <img src={banImg} w="full" />
                 </Box>
+                <Box w="full">
+                  <HStack marginTop={4}>
+                    <Button colorScheme="blue" variant="outline" w="50%" h="70px" borderRadius={20}>
+                      <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">一時停止</Text>
+                    </Button>
+                    <Button colorScheme="blue" variant="solid" w="80%" h="70px" borderRadius={20}>
+                      <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">終了</Text>
+                    </Button>
+                  </HStack>
+                </Box>
 
-                <Button colorScheme="blue" variant="solid" w="full" h="100px" borderRadius={10}>
-                  <Text fontSize="xl" fontWeight="bold" colorScheme="blue">投了</Text>
-                </Button>
               </VStack>
 
             </Box>
-
-            <Box p={4} bg={"white"} shadow="lg" rounded={20} w="1000px" h="full" margin={"0%"}></Box>
-          </HStack>
-        </AspectRatio>
+            {/* 右のプレイヤー */}
+            <Box p={4} bg={"white"} shadow="2xl" rounded="30px" w="1000px" h="full" margin={"0%"}>
+              <VStack h="full">
+                <HStack w="full">
+                  {gote}
+                  <Spacer />
+                </HStack>
+                <Spacer />
+                <Box shadow="xl" rounded=" full" padding={2}>
+                  <CountdownCircleTimer
+                    size={250}
+                    strokeWidth={10}
+                    isPlaying
+                    duration={timerSeconds}
+                    colors={["#5a97db", "#5a97db", "#f4d849", "#A30000"]}
+                    colorsTime={[60, 20, 10, 0]}
+                    onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+                  >
+                    {renderTime}
+                  </CountdownCircleTimer>
+                </Box>
+                <Box marginStart={10}>
+                  <Text as="span" fontSize={20} color="blue.400" fontWeight="bold">考慮回数 </Text>
+                  <Text as="span" fontSize={40} color="blue.400" fontWeight="bold">1</Text>
+                  <Text as="span" fontSize={20} color="blue.400" fontWeight="bold">回</Text>
+                </Box>
+                <Spacer />
+                <Button colorScheme="blue" shadow="lg" variant="outline" w="full" h="70px" borderRadius={20}>
+                  <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
+                </Button>
+              </VStack>
+            </Box>
+          </HStack >
+        </AspectRatio >
       </header >
     </div >
   )
