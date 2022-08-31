d317b60e7a7f882605463f06ced6da91dd797b45
Toyoshin <toyodasinngo@gmail.com>
2022-08-31 17:10:11 +0900

    Add animation for Result screen

--- frontend/proxy_server/src/pages/Result.js
+++ frontend/proxy_server/src/pages/Result.js
@@ -1,16 +1,32 @@
 import { AspectRatio, ChakraProvider, Spacer, VStack } from '@chakra-ui/react';
 import { Box, Button, Stack, HStack, Flex, Text, Center } from "@chakra-ui/react";
+import { TriangleUpIcon, TriangleDownIcon } from '@chakra-ui/icons';
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
 import NumberEasing from 'react-number-easing';
 import React, { useState } from 'react';
 import './../App.css';
-import banImg from './../images/ban.png';
+import './Result.css';
+
+
+const upIcon = <TriangleUpIcon w="50px" h="50px" color="red.500" />;
+const downIcon = <TriangleDownIcon w="50px" h="50px" color="blue.500" />;
 
 const Result = () => {
-    var [leftScore, setLeftScore] = useState(1500);
-    var [rightScore, setRightScore] = useState(1500);
+    var winner = "left";
+    var oldLeftScore = 1532;
+    var oldRightScore = 1490;
     var newleftScore = 1600;
-    var newrightScore = 1400;
+    var newrightScore = 1420;
+    var [leftScore, setLeftScore] = useState(oldLeftScore);
+    var [rightScore, setRightScore] = useState(oldRightScore);
+    let leftDist = newleftScore - oldLeftScore;
+    let rightDist = newrightScore - oldRightScore;
+    //do after 1 seconds
+    setTimeout(() => {
+        setLeftScore(newleftScore); setRightScore(newrightScore);
+    }, 1 * 1000);
+
+
     return (
         <div className="App">
             <header className="App-header">
@@ -21,16 +37,27 @@ const Result = () => {
                         <Flex w="full" align="center">
                             <Spacer></Spacer>
                             <VStack>
-                                <Text p="10px" fontSize="50px" fontWeight="bold" color="black">WINNER</Text>
+                                <Text p="10px" fontSize="50px" fontWeight="bold" color={(winner == "left") ? "red.500" : "blue.500"} >{(winner == "left") ? "WINNER" : "LOSER"}</Text>
                                 <Box p={4} transition="0.5s" bg="white" border="2px" borderColor="white" shadow="xl" rounded="41px" w="500px" h="300px" margin={"0%"}>
-                                    <HStack h="full" >
-                                        <Spacer />
-                                        <Text fontSize="50px" fontWeight="bold" color="red">上三角</Text>
-                                        <p style={{ fontWeight: "700", fontSize: "50px" }}><NumberEasing speed={3000} style="font-weight: bold;"
-                                            ease='quintInOut' value={leftScore} fontSize="50px" color="black" /></p>
-
-                                        <Spacer />
-                                    </HStack>
+                                    <VStack spacing="0" h="full" >
+                                        <Box bg={"black"} rounded="full" w="full" h="50px">
+                                            <Center h='100%'>
+                                                <Text letterSpacing={2} fontSize="25px" fontWeight="bold" color="white">RATING</Text>
+                                            </Center>
+                                        </Box>
+                                        <HStack paddingTop="20px">
+                                            <Spacer />
+                                            {(leftDist >= 0) ? upIcon : downIcon}
+                                            <p style={{ fontWeight: "700", fontSize: "70px" }}><NumberEasing speed={Math.abs(leftDist * 50)} style="font-weight: bold;"
+                                                ease='quintInOut' value={leftScore} fontSize="50px" color="black" /></p>
+                                            <Spacer />
+                                        </HStack>
+                                        <HStack  >
+                                            <Spacer />
+                                            <p class="diffText" >{(leftDist >= 0) ? "+" + leftDist : leftDist}</p>
+                                            <Spacer />
+                                        </HStack>
+                                    </VStack>
                                 </Box>
                             </VStack>
 
@@ -40,26 +67,39 @@ const Result = () => {
                         <Flex w="full" align="center">
                             <Spacer></Spacer>
                             <VStack>
-                                <Text p="10px" fontSize="50px" fontWeight="bold" color="black">LOSER</Text>
+                                <Text p="10px" fontSize="50px" fontWeight="bold" color={(winner == "right") ? "red.500" : "blue.500"} >{(winner == "right") ? "WINNER" : "LOSER"}</Text>
                                 <Box p={4} transition="0.5s" bg="white" border="2px" borderColor="white" shadow="xl" rounded="41px" w="500px" h="300px" margin={"0%"}>
-                                    <HStack h="full">
-                                        <Spacer />
-                                        <Text fontSize="50px" fontWeight="bold" color="blue">下三角</Text>
-                                        <p style={{ fontWeight: "700", fontSize: "50px" }}><NumberEasing speed={3000} style="font-weight: bold;"
-                                            ease='quintInOut' value={rightScore} fontSize="50px" color="black" /></p>
-                                        <Spacer />
-                                    </HStack>
+                                    <VStack spacing="0" h="full" >
+                                        <Box bg={"white"} border="2px" borderColor="black" rounded=" full" w="full" h="50px">
+                                            <Center h='100%'>
+                                                <Text letterSpacing={2} fontSize="25px" fontWeight="bold" color="black">RATING</Text>
+                                            </Center>
+                                        </Box>;
+                                        <HStack paddingTop="20px">
+                                            <Spacer />
+                                            {(rightDist >= 0) ? upIcon : downIcon}
+                                            <p style={{ fontWeight: "700", fontSize: "70px" }}>
+                                                <NumberEasing speed={Math.abs(rightDist * 50)} style="font-weight: bold;"
+                                                    ease='quintInOut' value={rightScore} fontSize="50px" color="black" /></p>
+                                            <Spacer />
+                                        </HStack>
+                                        <HStack  >
+                                            <Spacer />
+                                            <p class="diffText" >{(rightDist >= 0) ? "+" + rightDist : rightDist}</p>
+                                            <Spacer />
+                                        </HStack>
+                                    </VStack>
                                 </Box>
                             </VStack>
                             <Spacer></Spacer>
                         </Flex>
 
                     </HStack >
-                    <Button m="20px" colorScheme=" blue" variant="solid" w="400px" h="70px" borderRadius={25} onClick={() => { setLeftScore(newleftScore); setRightScore(newrightScore); }}>
+                    <Button m="20px" colorScheme=" blue" variant="solid" w="400px" h="70px" borderRadius={25} onClick={() => { }}>
                         <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">終了</Text>
                     </Button>
                 </Box>
-            </header>
+            </header >
         </div >
     )
 }
