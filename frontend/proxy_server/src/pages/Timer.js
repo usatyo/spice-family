a1fe28b8c0ed9f92040088637f567db1ac0248b5
Toyoshin <toyodasinngo@gmail.com>
2022-09-01 23:10:20 +0900

    タイマー画面が横並びになる問題の修正

--- frontend/proxy_server/src/pages/Timer.js
+++ frontend/proxy_server/src/pages/Timer.js
@@ -77,104 +77,107 @@ const Timer = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Box paddingLeft="40px" w="full">
-          <Text className='Title1' >Timer</Text>
-          <Text className='normal' >対局を行いましょう。</Text>
-        </Box>
-        <AspectRatio transform="perspective(600px) rotateX(0deg) scale(1,1)" w="full" ratio={16 / 9} >
-          <HStack p={10} spacing={4}>
-            {/* 左のプレイヤー(playState=0) */}
-            <Box p={4} transition="0.5s" bg="white" border="2px" borderColor={(playState == "left" && isPause == false) ? "white" : "white"} shadow={(playState == "left" && isPause == false) ? "xl" : "sm"} rounded="41px" w="1000px" h="full" margin={"0%"}>
-              <VStack h="full">
-                <HStack w="full">
-                  {leftBadge}
-                  <Spacer />
-                </HStack>
-                <Spacer />
-                <Box transition="0.5s" shadow={(playState == "left" && isPause == false) ? "xl" : "sm"} rounded=" full" padding={2}>
-                  <CountdownCircleTimer
-                    size={250}
-                    strokeWidth={10}
-                    isPlaying={(playState == "left" && isPause == false)}
-                    duration={timerSeconds}
-                    colors={(playState == "left" && isPause == false) ? ["#5a97db", "#5a97db", "#f4d849", "#A30000"] : ["#777777", "#777777", "#777777", "#777777"]}
-                    colorsTime={[60, 20, 10, 0]}
-                    onComplete={() => ({ shouldRepeat: true, delay: 1 })}
-                  >
-                    {renderTime}
-                  </CountdownCircleTimer>
-                </Box>
-                <Box marginStart={10}>
-                  <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">考慮回数 </Text>
-                  <Text as="span" fontSize={40} color="blue.500" fontWeight="bold">1</Text>
-                  <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">回</Text>
-                </Box>
-                <Spacer />
-                <Button transition="0.5s" colorScheme="blue" shadow="lg" variant="outline" border="2px" w="full" h="70px" borderRadius={25} disabled={(playState != "left" || isPause != false)} onClick={() => { setPlayState("right"); setPlayCount(playCount + 1) }}>
-                  <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
-                </Button>
-              </VStack>
-            </Box>
-            {/* 真ん中のところ */}
-            <Box w="full" h="full" >
-              <VStack spacing={0} h="full">
-                <>{(isPause) ? pausing : ((playState == "left") ? leftBadge : rightBadge)}</>
-                <Box>
-                  <Text as="span" fontSize={60} color="blue.500" fontWeight="bold">{playCount}</Text>
-                  <Text as="span" fontSize={30} color="blue.500" fontWeight="bold">手目</Text>
-                </Box>
-                <Box p={10} bg="white" shadow="lg" borderRadius={10} padding="10px">
-                  <img src={banImg} w="full" />
-                </Box>
-                <Box w="full">
-                  <HStack marginTop={4}>
-                    <Button colorScheme="blue" border="2px" variant="outline" w="50%" h="70px" borderRadius={25} onClick={() => { setIsPause(!isPause) }}>
-                      <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">{(isPause ? "再開" : "一時停止")}</Text>
-                    </Button>
-                    <Button colorScheme="blue" variant="solid" w="80%" h="70px" borderRadius={25} onClick={onOpen}>
-                      <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">終了</Text>
-                    </Button>
-                    {endModal(isOpen, onClose, playState)}
+        <Stack h="full" w="full">
+          <Box paddingLeft="40px" w="full">
+            <Text className='Title1' >Timer</Text>
+            <Text className='normal' >対局を行いましょう。</Text>
+          </Box>
+          <AspectRatio transform="perspective(600px) rotateX(0deg) scale(1,1)" w="full" ratio={16 / 9} >
+            <HStack p={10} spacing={4}>
+              {/* 左のプレイヤー(playState=0) */}
+              <Box p={4} transition="0.5s" bg="white" border="2px" borderColor={(playState == "left" && isPause == false) ? "white" : "white"} shadow={(playState == "left" && isPause == false) ? "xl" : "sm"} rounded="41px" w="1000px" h="full" margin={"0%"}>
+                <VStack h="full">
+                  <HStack w="full">
+                    {leftBadge}
+                    <Spacer />
                   </HStack>
-                </Box>
-
-              </VStack>
-
-            </Box>
-            {/* 右のプレイヤー */}
-            <Box p={4} transition="0.5s" bg="white" border="2px" borderColor={(playState == "right" && isPause == false) ? "white" : "white"} shadow={(playState == "right" && isPause == false) ? "xl" : "sm"} rounded="41px" w="1000px" h="full" margin={"0%"}>
-              <VStack h="full">
-                <HStack w="full">
-                  {rightBadge}
                   <Spacer />
-                </HStack>
-                <Spacer />
-                <Box transition="0.5s" shadow={(playState == "right" && isPause == false) ? "xl" : "sm"} rounded="full" padding={2}>
-                  <CountdownCircleTimer
-                    size={250}
-                    strokeWidth={10}
-                    isPlaying={(playState == "right" && isPause == false)}
-                    duration={timerSeconds}
-                    colors={(playState == "right" && isPause == false) ? ["#5a97db", "#5a97db", "#f4d849", "#A30000"] : ["#777777", "#777777", "#777777", "#777777"]}
-                    colorsTime={[60, 20, 10, 0]}
-                    onComplete={() => ({ shouldRepeat: true, delay: 1 })}
-                  >
-                    {renderTime}
-                  </CountdownCircleTimer>
-                </Box>
-                <Box marginStart={10}>
-                  <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">考慮回数 </Text>
-                  <Text as="span" fontSize={40} color="blue.500" fontWeight="bold">1</Text>
-                  <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">回</Text>
-                </Box>
-                <Spacer />
-                <Button transition="0.5s" colorScheme="blue" shadow="lg" variant="outline" border="2px" w="full" h="70px" borderRadius={25} disabled={(playState != "right" || isPause != false)} onClick={() => { setPlayState("left"); setPlayCount(playCount + 1) }}>
-                  <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
-                </Button>
-              </VStack>
-            </Box>
-          </HStack >
-        </AspectRatio >
+                  <Box transition="0.5s" shadow={(playState == "left" && isPause == false) ? "xl" : "sm"} rounded=" full" padding={2}>
+                    <CountdownCircleTimer
+                      size={250}
+                      strokeWidth={10}
+                      isPlaying={(playState == "left" && isPause == false)}
+                      duration={timerSeconds}
+                      colors={(playState == "left" && isPause == false) ? ["#5a97db", "#5a97db", "#f4d849", "#A30000"] : ["#777777", "#777777", "#777777", "#777777"]}
+                      colorsTime={[60, 20, 10, 0]}
+                      onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+                    >
+                      {renderTime}
+                    </CountdownCircleTimer>
+                  </Box>
+                  <Box marginStart={10}>
+                    <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">考慮回数 </Text>
+                    <Text as="span" fontSize={40} color="blue.500" fontWeight="bold">1</Text>
+                    <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">回</Text>
+                  </Box>
+                  <Spacer />
+                  <Button transition="0.5s" colorScheme="blue" shadow="lg" variant="outline" border="2px" w="full" h="70px" borderRadius={25} disabled={(playState != "left" || isPause != false)} onClick={() => { setPlayState("right"); setPlayCount(playCount + 1) }}>
+                    <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
+                  </Button>
+                </VStack>
+              </Box>
+              {/* 真ん中のところ */}
+              <Box w="full" h="full" >
+                <VStack spacing={0} h="full">
+                  <>{(isPause) ? pausing : ((playState == "left") ? leftBadge : rightBadge)}</>
+                  <Box>
+                    <Text as="span" fontSize={60} color="blue.500" fontWeight="bold">{playCount}</Text>
+                    <Text as="span" fontSize={30} color="blue.500" fontWeight="bold">手目</Text>
+                  </Box>
+                  <Box p={10} bg="white" shadow="lg" borderRadius={10} padding="10px">
+                    <img src={banImg} w="full" />
+                  </Box>
+                  <Box w="full">
+                    <HStack marginTop={4}>
+                      <Button colorScheme="blue" border="2px" variant="outline" w="50%" h="70px" borderRadius={25} onClick={() => { setIsPause(!isPause) }}>
+                        <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">{(isPause ? "再開" : "一時停止")}</Text>
+                      </Button>
+                      <Button colorScheme="blue" variant="solid" w="80%" h="70px" borderRadius={25} onClick={onOpen}>
+                        <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">終了</Text>
+                      </Button>
+                      {endModal(isOpen, onClose, playState)}
+                    </HStack>
+                  </Box>
+
+                </VStack>
+
+              </Box>
+              {/* 右のプレイヤー */}
+              <Box p={4} transition="0.5s" bg="white" border="2px" borderColor={(playState == "right" && isPause == false) ? "white" : "white"} shadow={(playState == "right" && isPause == false) ? "xl" : "sm"} rounded="41px" w="1000px" h="full" margin={"0%"}>
+                <VStack h="full">
+                  <HStack w="full">
+                    {rightBadge}
+                    <Spacer />
+                  </HStack>
+                  <Spacer />
+                  <Box transition="0.5s" shadow={(playState == "right" && isPause == false) ? "xl" : "sm"} rounded="full" padding={2}>
+                    <CountdownCircleTimer
+                      size={250}
+                      strokeWidth={10}
+                      isPlaying={(playState == "right" && isPause == false)}
+                      duration={timerSeconds}
+                      colors={(playState == "right" && isPause == false) ? ["#5a97db", "#5a97db", "#f4d849", "#A30000"] : ["#777777", "#777777", "#777777", "#777777"]}
+                      colorsTime={[60, 20, 10, 0]}
+                      onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+                    >
+                      {renderTime}
+                    </CountdownCircleTimer>
+                  </Box>
+                  <Box marginStart={10}>
+                    <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">考慮回数 </Text>
+                    <Text as="span" fontSize={40} color="blue.500" fontWeight="bold">1</Text>
+                    <Text as="span" fontSize={20} color="blue.500" fontWeight="bold">回</Text>
+                  </Box>
+                  <Spacer />
+                  <Button transition="0.5s" colorScheme="blue" shadow="lg" variant="outline" border="2px" w="full" h="70px" borderRadius={25} disabled={(playState != "right" || isPause != false)} onClick={() => { setPlayState("left"); setPlayCount(playCount + 1) }}>
+                    <Text fontSize="2xl" fontWeight="bold" colorScheme="blue">着手</Text>
+                  </Button>
+                </VStack>
+              </Box>
+            </HStack >
+          </AspectRatio >
+        </Stack>
+
       </header >
 
     </div >
