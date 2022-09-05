01cb56f39f82ce9c5e01180e62292654b818e5fa
usatyo <yu0424.math8128@gmail.com>
2022-09-05 16:48:14 +0900

    make api base

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,51 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Form, status
+from fastapi.responses import JSONResponse
+import uvicorn
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    # TODO: フロントエンドデプロイしたらそのURLも入れる
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "this is root"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id):
-    return {"item_id": item_id}
+@app.post("/post/board")
+def _():
+    return {}
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.post("/post/move")
+def _():
+    return {}
 
-@app.get("/items/")
-def read_query_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+
+@app.post("/post/result")
+def _():
+    return {}
+
+
+@app.get("/get/rate")
+def _():
+    return {}
+
+
+@app.get("/get/all_rate")
+def _():
+    return {}
