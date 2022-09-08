94890bbdd3db0f930132b5d7322e8d9e6f7f6e4f
usatyo <yu0424.math8128@gmail.com>
2022-09-08 22:33:16 +0900

    make all api

--- backend/main.py
+++ backend/main.py
@@ -4,21 +4,24 @@ import uvicorn
 import cv2
 import shutil
 import os
+from utility import calc_rate
 from database import (
     initialize,
     update_rate,
     get_all_pair,
     get_all_rate,
     get_current_rate,
+    update_result,
+    get_all_result,
 )
 from decide_color import color_array
 from detect_board import det_board
 from correct_board import cor_board
-from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 
 
 initialize()
+update_rate("aaa", 1234)
 app = FastAPI()
 
 origins = [
@@ -67,7 +70,21 @@ def _(
 
 
 @app.post("/post/result")
-def _():
+def _(
+    black: str,
+    white: str,
+    result: int,
+):
+    update_result(black, white, result)
+    b_rate = get_current_rate(black)
+    w_rate = get_current_rate(white)
+    if result == 1:
+        b_rate, w_rate = calc_rate(b_rate, w_rate, 0)
+    elif result == -1:
+        w_rate, b_rate = calc_rate(w_rate, b_rate, 0)
+
+    update_rate(black, b_rate)
+    update_rate(white, w_rate)
     return {}
 
 
@@ -76,7 +93,7 @@ def _(id: str):
     return get_current_rate(id)
 
 
-@app.get("/get/rate_hist")
+@app.get("/get/all_rate")
 def _(id: str):
     return get_all_rate(id)
 
@@ -84,3 +101,8 @@ def _(id: str):
 @app.get("/get/all_name")
 def _():
     return get_all_pair()
+
+
+@app.get("/get/result")
+def _():
+    return get_all_result()
