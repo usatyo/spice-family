f3b51254c70c7d1f87a2951d3fe2cd5cc81d74a8
usatyo <yu0424.math8128@gmail.com>
2022-09-16 22:10:19 +0900

    done util

--- frontend/proxy_server/src/utils/utils.js
+++ frontend/proxy_server/src/utils/utils.js
@@ -1,73 +1,62 @@
 import axios from "axios"
-import { useContext } from "react"
 
 const baseUrl = "http://localhost:8000"
 
-const {
-  gameId,
-  setGameId,
-} = useContext(AppContext)
 
 export const postNameAndId = async (user_id, name) => {
-  return await axios.post(baseUrl + `/post/name?user_id=${user_id}&name=${name}`)
-    .then((res) => {
-      console.log("done name and id")
-    })
-    .catch((err) => {
-      console.log("err:", err)
-    })
+  try {
+    const res = await axios.post(baseUrl + `/post/name?user_id=${user_id}&name=${name}`)
+    console.log(res)
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
 
 export const postNewGame = async (black_id, white_id) => {
-  return await axios.post(baseUrl + `/post/start_game?black=${black_id}&white=${white_id}`)
-    .then((res) => {
-      setGameId(res.data.game_id)
-      console.log("done new game")
-    })
-    .catch((err) =>{
-      console.log("err:", err)
-    })
+  try {
+    const res = await axios.post(baseUrl + `/post/start_game?black=${black_id}&white=${white_id}`)
+    console.log(res)
+    return res
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
 
-export const postResult = async (result) => {
-  return await axios.post(baseUrl + `/post/result?game_id=${gameId}&result=${result}`)
-    .then((res) => {
-      console.log("done result")
-    })
-    .catch((err) => {
-      console.log("err:", err)
-    })
+export const postResult = async (game_id, result) => {
+  try {
+    const res = await axios.post(baseUrl + `/post/result?game_id=${game_id}&result=${result}`)
+    console.log(res)
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
 
 export const getRateFromId = async (user_id) => {
-  return await axios.get(baseUrl + `/get/rate?id=${user_id}`)
-    .then((res) => {
-      console.log("done rate")
-      return res.data.rate
-    })
-    .catch((err) => {
-      console.log("err:", err)
-    })
+  try {
+    const res = await axios.get(baseUrl + `/get/rate?id=${user_id}`)
+    console.log(res)
+    return res
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
 
 export const getAllRate = async (user_id) => {
-  return await axios.get(baseUrl + `/get/all_rate?id=${user_id}`)
-    .then((res) => {
-      console.log("done all rate")
-      return res.data
-    })
-    .catch((err) => {
-      console.log("err:", err)
-    })
+  try {
+    const res = await axios.get(baseUrl + `/get/rate_hist?id=${user_id}`)
+    console.log(res)
+    return res
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
 
 export const getResult = async (user_id) => {
-  return await axios.get(baseUrl + `/get/result?id=${user_id}`)
-    .then((res) => {
-      console.log("done all result")
-      return res.data
-    })
-    .catch((err) => {
-      console.log("err:", err)
-    })
+  try {
+    const res = await axios.get(baseUrl + `/get/result?id=${user_id}`)
+    console.log(res)
+    return res
+  } catch (err) {
+    console.log("err:", err)
+  }
 }
