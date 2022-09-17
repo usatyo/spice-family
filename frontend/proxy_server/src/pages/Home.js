3965b9b07231f259b00652c7f6a7a0cee2b27e39
Toyoshin <toyodasinngo@gmail.com>
2022-09-17 18:50:00 +0900

    Merge branch 'main' of https://github.com/usatyo/spice-family

--- frontend/proxy_server/src/pages/Home.js
+++ frontend/proxy_server/src/pages/Home.js
@@ -10,20 +10,22 @@ const Home = () => {
     return (
         <div className="App" >
             <header className="App-header">
-                <VStack ml={100} mt={50}>
+                <VStack ml={100} mt={50} spacing={10}>
                     <HStack mb={30}>
-                        <Link to='/prepare'>
-                            <Button p={4} bg={"white"} shadow="lg" rounded="20px" w="120px" h="120px">
-                                <Image src={TimerSvg} h={75} />
-                            </Button>
-                        </Link>
+
+
                         <Box pl={270} pr={230} color={'#59A4CB'} fontSize="7xl" fontWeight="bold" >プロ棋士サーバ</Box>
                         <Spacer></Spacer>
-                        <Box pt={3} pl={4} bg={"white"} color={"black"} fontSize={17} textAlign={"left"} shadow="lg" rounded="10px" w="180px" h="50px">北海道大学囲碁部</Box>
                     </HStack>
+
                     <Stack p={12} bg={"white"} shadow="lg" rounded="20px" w="900px" h="450px">
                         <MyChart />
                     </Stack>
+                    <Link to="/prepare" >
+                        <Button colorScheme="blue" variant="solid" w="100%" h="80px" borderRadius="40px" >
+                            <Text fontSize="2xl" fontWeight="bold" colorScheme="blue"><Image src={TimerSvg} h={10} />対局開始</Text>
+                        </Button>
+                    </Link>
                 </VStack>
             </header>
         </div>
